dc7e7d5ec93b892259e99daab3a1db091164cac5
Gregory Fefelov <gfv@zoidberg.md>
2013-11-06 02:14:16 +0400

    amazing flight

---
+++ sovi.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int main(int argc, char** argv) {
+    int i = 0;
+    char wave[130];
+    for(int j = 0; j < 129; j++) {
+        wave[j] = ' ';
+    }
+    wave[129] = 0;
+    while (1) {
+        wave[64] = '|';
+        wave[32] = '[';
+        wave[96] = ']';
+        unsigned char c;
+        i++;
+        scanf("%c", &c);
+        if (i%2 == 0) continue;
+        printf("\n");
+        int rv = (c/2);
+        wave[rv] = '%';
+
+        printf("%s", wave);
+        wave[rv] = ' ';
+        if (rv > 96 || rv < 32) {
+            printf("\033[01;31m%d\033[00m RED", rv);
+        } else {
+            printf("%d", rv);
+        }
+    }
+
+    return 0;
+}
